ab9f42f25a57da4ed7bce4c5cc2efef86c232165
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-06 08:42:56 +0300

    added a constructor in the new model class for the quote

---
+++ src/app/quote-form.component.ts
@@ -0,0 +1,9 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component } from '@angular/core';
+@Componet ({
+    selector:'',
+    template:'<h4>Enter your quote</h4>'
+})
+export class FormComponent {
+    
+}
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,11 @@
 export class Quote {
+  showDescription: boolean;
+  constructor(
+    public id: number,
+    public author: string,
+    public description: string,
+    public name: string){
+    this.showDescription=false;
+
+}
 }
